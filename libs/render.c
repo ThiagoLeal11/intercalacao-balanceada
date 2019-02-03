9973c0f2c11317b5d45a75a925dc04a85847815e
Vinicios Barretos <40036992+viniciosbarretos@users.noreply.github.com>
2019-02-03 19:20:39 -0200

    User Interface added

---
+++ libs/render.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "render.h"
+
+#define stdoutSize = 80;
+
+// Clean buffer after a text input
+void cleanBuffer() {
+    int i;
+    while ((i = getchar()) != '\n');
+}
+
+void waitForChar(const char *message) {
+    cleanBuffer();
+    printf("%s", message);
+    getchar();
+}
+
+
+// Clear screen both windows and unix
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void printSequence(const char *str, int times) {
+    while ( times > 0 ) {
+        printf("%s", str);
+        times--;
+    }
+}
+
+void printDivision(const char *divider, const char *sequence, int size, ...) {
+    int i;
+
+    va_list list;
+    va_start(list, size);
+
+    for (i = 0; i < size; i ++) {
+        printf("%s", divider);
+        printSequence(sequence, va_arg(list, int));
+    }
+
+    printf("%s\n", divider);
+}
+
+// Todo better header.
+void printHeader(const char *str) {
+    clearScreen();
+    unsigned size = (unsigned) strlen(str);
+    printf("\n");
+    printSequence("-", size + 8);
+    printf("\n-   %s   -\n", str);
+    printSequence("-", size + 8);
+    printf("\n");
+}
+
+void printMenu() {
+    clearScreen();
+
+    printf("\n");
+    printHeader("Balanced Interleaving");
+
+    printf("\n\nChoose an option entering the corresponding number:\n\n");
+    printf("[1] - Show Running Settings\n");
+    printf("[2] - Define Memory Size\n");
+    printf("[3] - Define Input File\n");
+    printf("[4] - Define Output File\n");
+    printf("[5] - Start Sorting\n");
+    printf("[0] - Exit.\n\n");
+
+    printf("Option: ");
+}
---
+++ libs/render.h
@@ -0,0 +1,14 @@
+#ifndef SGBD_ARVORE_B_MAIS_RENDER_H
+#define SGBD_ARVORE_B_MAIS_RENDER_H
+
+void clearScreen();
+void cleanBuffer();
+void waitForChar(const char *message);
+void printHeader(const char *str);
+void printStudentRow(int id, const char *name, const char *email, int age, char status);
+void printStudentBlock(int id, const char *name, const char *email, int age, char status);
+void printStudentDivision();
+void printStudentRowHeader();
+void printMenu();
+
+#endif //SGBD_ARVORE_B_MAIS_RENDER_H
